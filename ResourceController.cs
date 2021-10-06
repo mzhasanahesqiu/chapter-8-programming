aeab1e9f0153c8ef7f20c7ab5b4feee9fe5c3ad4
Mau'idzoh Hasanah <1910631170095@student.unsika.ac.id>
2021-10-07 00:53:54 +0700

    Implementasi Firebase Sederhana

---
+++ ResourceController.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceController : MonoBehaviour
+{
+    public Button ResourceButton;
+    public Image ResourceImage;
+    public Text ResourceDescription;
+    public Text ResourceUpgradeCost;
+    public Text ResourceUnlockCost;
+
+    private ResourceConfig _config;
+
+    private int _index;
+    private int _level
+    {
+        set
+        {
+            UserDataManager.Progress.ResourcesLevels[_index] = value;
+            UserDataManager.Save(true);
+        }
+
+        get
+        {
+            if (!UserDataManager.HasResources(_index))
+            {
+                return 1;
+            }
+            
+            return UserDataManager.Progress.ResourcesLevels[_index];
+        }
+    }
+
+    public bool IsUnlocked { get; private set; }
+
+    private void Start()
+    {
+        ResourceButton.onClick.AddListener(() =>
+        {
+            if (IsUnlocked)
+            {
+                UpgradeLevel();
+            }
+            else
+            {
+                UnlockResource();
+            }
+        });
+    }
+
+    public void SetConfig(int index, ResourceConfig config)
+    {
+        _index = index;
+        _config = config;
+        
+        ResourceDescription.text = $"{ _config.Name } Lv. { _level }\n+{ GetOutput().ToString("0") }";
+        ResourceUnlockCost.text = $"Unlock Cost\n{ _config.UnlockCost }";
+        ResourceUpgradeCost.text = $"Upgrade Cost\n{ GetUpgradeCost() }";
+
+        SetUnlocked(_config.UnlockCost == 0 || UserDataManager.HasResources(_index));
+    }
+
+    public double GetOutput()
+    {
+        return _config.Output * _level;
+    }
+
+    public double GetUpgradeCost()
+    {
+        return _config.UpgradeCost * _level;
+    }
+
+    public double GetUnlockCost()
+    {
+        return _config.UnlockCost;
+    }
+
+    public void UpgradeLevel()
+    {
+        double upgradeCost = GetUpgradeCost();
+        if (UserDataManager.Progress.Gold < upgradeCost)
+        {
+            return;
+        }
+
+        GameManager.Instance.AddGold(-upgradeCost);
+        _level++;
+
+        ResourceUpgradeCost.text = $"Upgrade Cost\n{ GetUpgradeCost() }";
+        ResourceDescription.text = $"{ _config.Name } Lv. { _level }\n+{ GetOutput().ToString("0") }";
+        AnalyticsManager.LogUpgradeEvent(_index, _level);
+    }
+
+    public void UnlockResource()
+    {
+        double unlockCost = GetUnlockCost();
+        if (UserDataManager.Progress.Gold < unlockCost)
+        {
+            return;
+        }
+
+        SetUnlocked(true);
+        GameManager.Instance.ShowNextResource();
+        AchievementController.Instance.UnlockAchievement(AchievementType.UnlockResource, _config.Name);
+        AnalyticsManager.LogUnlockEvent(_index);
+    }
+
+    public void SetUnlocked(bool unlocked)
+    {
+        IsUnlocked = unlocked;
+        if (unlocked)
+        {
+            if (!UserDataManager.HasResources(_index))
+            {
+                UserDataManager.Progress.ResourcesLevels.Add(_level);
+                UserDataManager.Save(true);
+            }
+        }
+
+        ResourceImage.color = IsUnlocked ? Color.white : Color.grey;
+        ResourceUnlockCost.gameObject.SetActive(!unlocked);
+        ResourceUpgradeCost.gameObject.SetActive(unlocked);
+    }
+}
