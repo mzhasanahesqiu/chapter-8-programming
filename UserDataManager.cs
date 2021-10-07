bf5b5ea0760856b20a7e8caa6832ac26332f0d6d
mzhasanahesqiu <75381611+mzhasanahesqiu@users.noreply.github.com>
2021-10-07 14:59:48 +0700

    Update UserDataManager.cs

--- UserDataManager.cs
+++ UserDataManager.cs
@@ -1,88 +1,31 @@
-﻿using Firebase.Storage;
-using System.Collections;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 
 public static class UserDataManager
 {
     private const string PROGRESS_KEY = "Progress";
 
-    public static UserProgressData Progress = new UserProgressData();
+    public static UserProgressData Progress = new UserProgressData ();
 
-    public static void LoadFromLocal()
+    public static void Load ()
     {
-        if (!PlayerPrefs.HasKey(PROGRESS_KEY))
+        if (!PlayerPrefs.HasKey (PROGRESS_KEY))
         {
-            Save(true);
+            Save ();
         }
         else
         {
-            string json = PlayerPrefs.GetString(PROGRESS_KEY);
-            Progress = JsonUtility.FromJson<UserProgressData>(json);
+            string json = PlayerPrefs.GetString (PROGRESS_KEY);
+            Progress = JsonUtility.FromJson<UserProgressData> (json);
         }
     }
 
-    public static IEnumerator LoadFromCloud(System.Action onComplete)
+    public static void Save ()
     {
-        StorageReference targetStorage = GetTargetCloudStorage();
-
-        bool isCompleted = false;
-        bool isSuccessfull = false;
-        const long maxAllowedSize = 1024 * 1024;
-
-        targetStorage.GetBytesAsync(maxAllowedSize).ContinueWith((Task<byte[]> task) =>
-        {
-            if (!task.IsFaulted)
-            {
-                string json = Encoding.Default.GetString(task.Result);
-                Progress = JsonUtility.FromJson<UserProgressData>(json);
-                isSuccessfull = true;
-            }
-
-            isCompleted = true;
-        });
-
-        while (!isCompleted)
-        {
-            yield return null;
-        }
-        
-        if (isSuccessfull)
-        {
-            Save();
-        }
-
-        else
-        {
-            LoadFromLocal();
-        }
-
-        onComplete?.Invoke();
+        string json = JsonUtility.ToJson (Progress);
+        PlayerPrefs.SetString (PROGRESS_KEY, json);
     }
 
-    public static void Save(bool uploadToCloud = false)
-    {
-        string json = JsonUtility.ToJson(Progress);
-        PlayerPrefs.SetString(PROGRESS_KEY, json);
-
-        if (uploadToCloud)
-        {
-            AnalyticsManager.SetUserProperties("gold", Progress.Gold.ToString());
-            byte[] data = Encoding.Default.GetBytes(json);
-            StorageReference targetStorage = GetTargetCloudStorage();
-            targetStorage.PutBytesAsync(data);
-        }
-    }
-
-    private static StorageReference GetTargetCloudStorage()
-    {
-        string deviceID = SystemInfo.deviceUniqueIdentifier;
-        FirebaseStorage storage = FirebaseStorage.DefaultInstance;
-        return storage.GetReferenceFromUrl($"{storage.RootReference}/{deviceID}");
-    }
-
-    public static bool HasResources(int index)
+    public static bool HasResources (int index)
     {
         return index + 1 <= Progress.ResourcesLevels.Count;
     }
